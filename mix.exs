01289c43825f4db0fcd525e02816636412fa2629
Victor Borja <vborja@apache.org>
2016-09-25 02:40:10 -0500

    docs

--- mix.exs
+++ mix.exs
@@ -46,6 +46,8 @@ defmodule HappyWith.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 end
