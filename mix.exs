6969cd3b18ff9ca122ae59eb9550b4fd58ea32ea
Jason Axelson <jason.axelson@gmail.com>
2017-12-13 22:17:52 -1000

    Fix compile warnings
    Also update ex_doc since it was generating warnings as well.

--- mix.exs
+++ mix.exs
@@ -5,11 +5,11 @@ defmodule HappyWith.Mixfile do
     [app: :happy_with,
      version: "1.0.0",
      elixir: "~> 1.2",
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   def package do
